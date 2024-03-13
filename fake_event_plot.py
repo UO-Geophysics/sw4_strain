80b4df8d8caf23f7df4b770bbca9a3028bc5a871
Sydney Dybing <sydneydybing@d92-224.uoregon.edu>
2024-03-13 15:09:56 -0700

    Added input file and slurm script for new Talapas run with a rupture model instead of a point source

--- fake_event_plot.py
+++ fake_event_plot.py
@@ -7,13 +7,72 @@ Created on Fri Mar  8 14:45:38 2024
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from obspy.core import read, Stream
 
-path = ''
-data = np.genfromtxt('./VCI_example/ALBH4.txt')
+path = '/Users/sydneydybing/SW4/strain/rc_fake-results/'
+stas = ['B916', 'B917', 'B918', 'B921']
+test_stas = ['B917']
 
-plt.plot(data[:,0],data[:,1], label = 'E-W')
-plt.plot(data[:,0],data[:,2], label = 'N-S')
-plt.plot(data[:,0],data[:,3], label = 'Vertical')
-plt.xlabel('Time (s)')
-plt.ylabel('Velocity (m/s)')
-plt.legend()
+for sta in test_stas:
+    
+    xx = read(path + str(sta) + '.xx')
+    xy = read(path + str(sta) + '.xy')
+    xz = read(path + str(sta) + '.xz')
+    yy = read(path + str(sta) + '.yy')
+    yz = read(path + str(sta) + '.yz')
+    zz = read(path + str(sta) + '.zz')
+    
+    rms = np.sqrt((xx[0].data**2 + xy[0].data**2 + xz[0].data**2 + yy[0].data**2 + yz[0].data**2 + zz[0].data**2)/6)
+    
+    i = np.where(rms > 0)[0]
+    p_arr_idx = i[0]
+    
+    rms_st = xx.copy()
+    rms_st[0].data = rms
+    
+    rms_data = rms_st[0].data
+    rms_times = rms_st[0].times()
+    
+    mod_rms = rms_st.copy()
+    mod_rms_data = mod_rms[0].data
+    mod_rms_times = mod_rms[0].times()
+    
+    p_arr_strain = rms_data[p_arr_idx]
+    
+    mod_rms_data[:p_arr_idx] = p_arr_strain
+    
+    pst = mod_rms.copy()
+    
+    # Loop over samples
+    for k in range(0,len(mod_rms[0].data)): #avoid starting at zero
+
+        if k == 0:
+            strain = mod_rms[0].data[0]
+            max_strain = strain
+
+        else:
+            # Grab progressively longer windows and save the biggest strain
+            strain = mod_rms[0].data[:k+1] # Has to be k+1 because slicing doesn't include last one
+            max_strain = max(strain)
+
+        # Put peak strain back into the output stream
+        pst[0].data[k] = max_strain 
+    
+    plt.figure(dpi = 100)
+    plt.title(sta)
+    # plt.plot(rms_times, rms_data, color = 'green', label = 'RMS strain')
+    plt.plot(mod_rms_times, mod_rms_data, color = 'blue', label = 'RMS strain')
+    plt.plot(pst[0].times(), pst[0].data, color = 'red', label = 'Peak strain')
+    plt.semilogy()
+    plt.axvline(rms_st[0].times()[i[0]], color = 'gray', linestyle = '--', label = 'P-wave arrival')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Strain')
+    plt.xlim(0,20)
+    plt.legend()
+    plt.show()
+    
+    
+    
+    
+    
+    
